af2a01098e896e4401aa1d481252fc4dabc9931e
DashGuy999 <64654064+DashGuy999@users.noreply.github.com>
2022-06-04 20:00:21 -0400

    Working version

--- TIMER_TEST/src/main/java/frc/robot/ParametricFunctionSolver.java
+++ TIMER_TEST/src/main/java/frc/robot/ParametricFunctionSolver.java
@@ -12,6 +12,8 @@ import edu.wpi.first.math.Matrix;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import java.awt.Desktop;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -21,16 +23,12 @@ public class ParametricFunctionSolver {
     private final double[] endChunk = {-1,-1};
     private final double[] endPath = {-2,-2};
 
-    /*
+    
     private double[][] allCoordinates = 
     {{5,1, 0}, {2,-9, 1}, {-3,-3, 2}, {-10,10, 3}, endChunk,
-    {-10,10, 4}, {0,0, 5}, {4,-7, 6}, {9,4, 7}, endChunk,
+    {-10,10, 3}, {-15,0, 4}, {-8,-3, 5}, {-8,-8, 6}, endChunk,
     endPath};
-    */
-    private double[][] allCoordinates = 
-    {{0,0, 0}, {1,2, 1}, endChunk, 
-    {1,2, 2}, {1,-4, 3}, {3,-5, 4}, endChunk,
-    endPath};
+    
 
     private double[][] chunk = new double[4][3];
     private double[] coordinate = new double[3];
@@ -40,6 +38,7 @@ public class ParametricFunctionSolver {
     private SimpleMatrix B;
     private SimpleMatrix C;
     
+    private String queryString = "";
 
 
   public ParametricFunctionSolver() {  
@@ -82,47 +81,44 @@ public class ParametricFunctionSolver {
                 C.insertIntoThis(0, i, invA.mult(B)); 
             }
 
-            try{
-                SmartDashboard.putNumber("AX", C.get(0, 0));
-                SmartDashboard.putNumber("BX", C.get(1, 0));
-                SmartDashboard.putNumber("CX", C.get(2, 0));
-                //SmartDashboard.putNumber("DX", C.get(3, 0));
-                
-                SmartDashboard.putNumber("AY", C.get(0, 1));
-                SmartDashboard.putNumber("BY", C.get(1, 1));
-                SmartDashboard.putNumber("CY", C.get(2, 1));
-                //SmartDashboard.putNumber("DY", C.get(3, 1));
-            }
-            catch(Exception exception){
-
-            }
-
             
+
+            //Setting up query string
+            //Testing query string: ?u=1&x=1&x=2&y=1&y=2&br&u=4&x=2&x=3&y=4&y=2&
+            
+            queryString = queryString + "u=" + chunk[chunkLength-1][2] + "&";
+
+            for(int i = 0; i < C.numRows(); i++){
+                BigDecimal rounder = new BigDecimal(C.get(i, 0));
+                BigDecimal rounded = rounder.setScale(4, RoundingMode.FLOOR);
+                queryString = queryString + "x=" + rounded + "&";
+            }
+            for(int i = 0; i < C.numRows(); i++){
+                BigDecimal rounder = new BigDecimal(C.get(i, 1));
+                BigDecimal rounded = rounder.setScale(4, RoundingMode.FLOOR);
+                queryString = queryString + "y=" + rounded + "&";
+            }
+            queryString = queryString + "br&";
             
             chunkLength = 0;
         }
         SmartDashboard.putString("Trip", "After Loop");
+        
+        //Shave off extra br&
+        queryString = queryString.substring(0,queryString.length()-3);
+        
         //Opening Desmos
-
         
         try {
 
             URI desmosURI = new URI("https://dashguy999.github.io/DesmosTesting/");
 
-            
-            String queryString = "value= 3t^3+2t^2";
-
             //SmartDashboard.putString("desmosURI.getQuery before", desmosURI.getQuery());
 
-            String newQuery = desmosURI.getQuery();
-            if (newQuery == null) {
-                newQuery = queryString;
-            } else {
-                newQuery += "&" + queryString;  
-            }
+            
             
             URI newDesmosURI = new URI(desmosURI.getScheme(), desmosURI.getAuthority(),
-            desmosURI.getPath(), newQuery, desmosURI.getFragment());
+            desmosURI.getPath(), queryString, desmosURI.getFragment());
 
             SmartDashboard.putString("Generated URL", newDesmosURI.toString());
             SmartDashboard.putString("desmosURI.getQuery after", newDesmosURI.getQuery());
